436a6aa927cc35743e373ada244d8ec4e62f8ae2
MostafaGohar <mostafa-gohar@hotmail.com>
2015-03-07 12:24:49 +0200

    added error messages to signin/up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,13 +23,26 @@ class UsersController < ApplicationController
     end
   end
 
-  def create 
+  def create
+    @email = params[:user][:email]
+    @password = params[:user][:password]
+    @phone = params[:user][:phone]
+    @fname = params[:user][:fname]
+    @lname = params[:user][:lname]
+    @country = params[:user][:country]
+    @blank = false
+    @usedemail = false
+    if @email.blank? || @password.blank? || @phone.blank? || @fname.blank? || @lname.blank? || @country.blank?
+    @blank = true
+    render 'new'
+    return
+    end
   	  @user = User.new(params.require(:user).permit(:email, :password , :fname , :lname, :country, :phone ,:validated))
     if  @user.save
       log_in(@user)
       redirect_to "/users/#{@user.id}"
     else
-    render 'create'
+    render 'new'
 
   end 
   end 
@@ -40,16 +53,18 @@ class UsersController < ApplicationController
     
   end
   def login
+    @email = params[:login][:email]
+    @password = params[:login][:password]
     @blank = false
     @wrong = false
-    user = User.where(:email => params[:login][:email].downcase).first
+    user = User.where(:email => @email.downcase).first
     
-    if params[:login][:email].blank? || params[:login][:password].blank?
+    if @email.blank? || @password.blank?
       @blank = true
       render 'signin'
     return
     end
-if user!= nil && User.login?(params[:login][:email].downcase,params[:login][:password])
+if user!= nil && User.login?(@email.downcase,@password)
       log_in user
       redirect_to "/users/#{user.id}"
       return
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :ads, through: :bookmark
   has_one :seller
   has_one :buyer
+  validates_uniqueness_of :email
+  validates :fname, presence: true
+  validates :password, presence: true
+  validates :phone, presence: true
 
   def self.login?(email, password)
     member = User.where(email: email).take
