0f86a7c1bd337c1b1940bb91581323853222a1ba
amallasheen <amalwalied@gmail.com>
2015-04-04 20:47:52 +0200

    added company seller model and added input validation for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,8 +54,7 @@ class UsersController < ApplicationController
     end 
     if  @user.save
       log_in(@user)
- 
-      @buyer= Buyer.create(user_id: @user.id)
+      @buyer= Buyer.new(:user_id => @user.id)
       @buyer.save
            redirect_to "/users/#{@user.id}"
     else
@@ -114,6 +113,6 @@ class UsersController < ApplicationController
     redirect_to "/users/#{self.current_user.id}"
   end
   def inputValidation user
-  return user.email.match(/^\w+\W*\w*\@\w+\.\w+\$/) && user.fname.match(/^[[:alpha:]]+$/) && user.lname.match(/^[[:alpha:]]+$/) && user.country.match(/^[[:alpha:]]+$/) && user.phone.match(/^\+?+[[:digit:]]{,20}$/)
+  return user.email.match(/^[[:alpha:]]+[[:punct:]]?[[:alpha:]]*(@[[:alpha:]]+.[[:alpha:]]+){,5}$/) && user.fname.match(/^[[:alpha:]]+$/) && user.lname.match(/^[[:alpha:]]+$/) && user.country.match(/^[[:alpha:]]+$/) && user.phone.match(/^\+?+[[:digit:]]{,20}$/)
   end
 end
