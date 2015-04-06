36bb7ecb7d81f726acb5c51b08a75a0f07700566
RainbowZephyr <darkfeathers9@gmail.com>
2015-04-06 11:33:31 +0200

    switching

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -21,10 +21,13 @@ class MakesController < ApplicationController
   end
 
   def index
+    @makes = Make.all
   end
 
   def create
-    @make = Make.new(params.require(:make).permit(:purchase, :new, :imported, ad_attributes: [:id,:title, :description]))
+    @make = Make.new(params.require(:make).permit(:purchase, :new, :imported, ad_attributes: [:id,:title, :description], vehicle_attributes: [:id,:make, :model, :colour, :manyear, :country, :axles, :gearbox, :price ]))
+    @make.save
+    redirect_to '/makes/index'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :makes
+ # resources :makes
   get 'makes/new'
 
   get 'makes/edit'
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'makes/destroy'
 
   get 'makes/index'
+
+  post 'makes/new'
 
 resources :vehicles
   get 'vehicles/new'
