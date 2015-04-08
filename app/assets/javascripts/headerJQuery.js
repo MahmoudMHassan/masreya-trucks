38ad9186a8bd5888c7c38a0920a92b53b2a4f1bb
AhmedAldahshoury <ahmed.aldahshoury@gmail.com>
2015-04-08 12:36:38 +0200

    merging with master

--- app/assets/javascripts/headerJQuery.js
+++ app/assets/javascripts/headerJQuery.js
@@ -14,23 +14,44 @@ $(document).ready(function() {
 $(function(){
   if(window.location.href.indexOf("&make=")>-1){
     $('#searchwrapper').css("visibility","visible");
+    $('#trialbutton').css("visibility","visible");
+    $('#trialbutton1').css("visibility","visible");
+    $('#trialbutton2').css("visibility","visible");
+    $('#trialbutton3').css("visibility","visible");
+    $('#trialbutton4').css("visibility","visible");
+
     $('#header').css("height","270px");
     $('#body').css("margin-top","270px");
   }else{
     $('#searchwrapper').css("visibility","hidden");
+    $('#trialbutton').css("visibility","hidden");
+    $('#trialbutton1').css("visibility","hidden");
+    $('#trialbutton2').css("visibility","hidden");
+    $('#trialbutton3').css("visibility","hidden");
+    $('#trialbutton4').css("visibility","hidden");
     $('#header').css("height","120px");
     $('#body').css("margin-top","120px");
   }
 });
 $('#searchbutton').click(function(e){
-   
+
         if($('#header').outerHeight() == 121)
-        {	
+        {
 	    $('#searchwrapper').css("visibility","visible");
-            $('#header').stop().animate({
+	     $('#trialbutton').css("visibility","visible");
+	     $('#trialbutton1').css("visibility","visible");
+	     $('#trialbutton2').css("visibility","visible");
+	     $('#trialbutton3').css("visibility","visible");
+	     $('#trialbutton4').css("visibility","visible");
+	     $('#header').stop().animate({
                 height:'270px'
             },400);
 	    $('#searchwrapper').fadeIn();
+	    $('#trialbutton').fadeIn();
+	    $('#trialbutton1').fadeIn();
+	    $('#trialbutton2').fadeIn();
+	    $('#trialbutton3').fadeIn();
+	    $('#trialbutton4').fadeIn();
 	    $('#body').stop().animate({
                 marginTop:'270px'
             },400);
@@ -40,10 +61,15 @@ $('#searchbutton').click(function(e){
                 height:'120px'
             },400);
 	    $('#searchwrapper').fadeOut();
+	    $('#trialbutton').fadeOut();
+	    $('#trialbutton1').fadeOut();
+	    $('#trialbutton2').fadeOut();
+	    $('#trialbutton3').fadeOut();
+	    $('#trialbutton4').fadeOut();
 	    $('#body').stop().animate({
                 marginTop:'120px'
             },400);
-          
+
     }e.preventDefault();
 })});
- 
+
