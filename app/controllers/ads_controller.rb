e16bdfdbb79ca99be609610278d96e875f870d8a
MostafaGohar <mostafa-gohar@hotmail.com>
2015-04-20 13:41:35 +0200

    indentation in code and modified results page

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -12,9 +12,7 @@ class AdsController < ApplicationController
     @ads = Make.joins(:ad,:vehicle).limit(10).order('created_at DESC')
   end
 
-
   def new
-
     @ad = Ad.new
     @vehicle = Vehicle.new
     @make = Make.new
@@ -22,54 +20,46 @@ class AdsController < ApplicationController
     @heavytruck = Heavytruck.new
     @semitrailer = Semitrailer.new
     @semitrailertruck = Semitrailertruck.new
-
   end
 
   def create
     @ad = Ad.new(ad_params)
     if @ad.save
-    @vehicle = Vehicle.new(vehicle_params)
-    if @vehicle.save
-    @make = Make.new(user_id: self.current_user.id,vehicle_id: @vehicle.id,ad_id: @ad.id)
-    @make.update(make_params)
-    @make.save
+        @vehicle = Vehicle.new(vehicle_params)
+        if @vehicle.save
+            @make = Make.new(user_id: self.current_user.id,vehicle_id: @vehicle.id,ad_id: @ad.id)
+            @make.update(make_params)
+            @make.save
 
-  # @picture = Picture.new(ad_id: @ad.id)
-   #@picture.update(picture_params)
-#     @picture = @ad.pictures.create!(params[:picture])
-#    @picture.save
+            if params[:van]
+            @van = Van.new(vehicle_id: @vehicle.id)
+            @van.update(van_params)
+            @van.save
+            elsif params[:heavytruck]
+            @heavytruck = Heavytruck.new(vehicle_id: @vehicle.id)
+            @heavytruck.update(heavytruck_params)
+            @heavytruck.save
+            elsif params[:semitrailer]
+            @semitrailer = Semitrailer.new(vehicle_id: @vehicle.id)
+            @semitrailer.update(semitrailer_params)
+            @semitrailer.save
+            elsif params[:semitrailertruck]
+            @semitrailertruck = Semitrailertruck.new(vehicle_id: @vehicle.id)
+            @semitrailertruck.update(semitrailertruck_params)
+            @semitrailertruck.save
+            end
+            redirect_to "/ads/#{@ad.id}"
 
-    if params[:van]
-      @van = Van.new(vehicle_id: @vehicle.id)
-      @van.update(van_params)
-      @van.save
-    elsif params[:heavytruck]
-      @heavytruck = Heavytruck.new(vehicle_id: @vehicle.id)
-      @heavytruck.update(heavytruck_params)
-      @heavytruck.save
-    elsif params[:semitrailer]
-      @semitrailer = Semitrailer.new(vehicle_id: @vehicle.id)
-      @semitrailer.update(semitrailer_params)
-      @semitrailer.save
-    elsif params[:semitrailertruck]
-      @semitrailertruck = Semitrailertruck.new(vehicle_id: @vehicle.id)
-      @semitrailertruck.update(semitrailertruck_params)
-      @semitrailertruck.save
-    end
-    redirect_to "/ads/#{@ad.id}"
+        else
+            flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
+            render 'new'
 
-   else
-     flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
-     render 'new'
-
-   end
+        end
     else 
-      flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
-     render 'new'
-   end
-  
+        flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
+        render 'new'
+    end
   end
-  
 
   def search
     @ads = Ad.search(params[:sort],params[:make],params[:model],params[:manyear],params[:country],params[:axles],params[:gearbox],params[:colour],params[:price],params[:capacity],params[:mileage],params[:type],params[:new],params[:imported],params[:purchase])
@@ -94,6 +84,7 @@ class AdsController < ApplicationController
     @bookmark.save
     redirect_to "/ads/#{params[:id]}"
   end
+  
   def  unbookmark
     Bookmark.where(:user_id => self.current_user.id,:ad_id => params[:id]).destroy_all
     redirect_to "/ads/#{params[:id]}"
@@ -109,7 +100,6 @@ class AdsController < ApplicationController
   def edit
     @ad = Ad.find(params[:id]) if Ad.exists?(params[:id])
     @make = Make.find_by_ad_id(params[:id])
-
     @vehicle = Vehicle.find(@make.vehicle_id)
     @seller = self.current_user
     @van = Van.find_by_vehicle_id(@vehicle.id)
@@ -121,7 +111,6 @@ class AdsController < ApplicationController
   def update
     @ad = Ad.find(params[:id]) if Ad.exists?(params[:id])
     @make = Make.find_by_ad_id(params[:id])
-
     @vehicle = Vehicle.find(@make.vehicle_id)
     @seller = self.current_user
     @van = Van.find_by_vehicle_id(@vehicle.id)
@@ -138,65 +127,60 @@ class AdsController < ApplicationController
       @vehicle.semitrailertruck.update(semitrailertruck_params)
     elsif @heavytruck !=nil
       @vehicle.heavytruck.update(heavytruck_params)
-
     end
-    if @vehicle.save
-    @ad.update(ad_params)
-    if @ad.save
 
-    @make.update(make_params)
-    @make.save
-    #connect = ActiveRecord::Base.connection();
-    #connect.execute(ActiveRecord::Base.send(:sanitize_sql_array, ["Update makes SET new = %s, purchase = %s, imported =%s WHERE ad_id =%s",params[:new],params[:purchase],params[:imported],@ad.id]))
-     #sql ="Update makes SET new = #{params[:new]}, purchase = #{params[:purchase]}, imported =  #{params[:imported]} WHERE ad_id = #{@ad.id}"
-     # ActiveRecord::Base.connection.execute(sql)
-redirect_to "/ads/#{@ad.id}"
-    else 
-      flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
-     render 'edit'
-   end
+    if @vehicle.save
+        @ad.update(ad_params)
+        if @ad.save
+            @make.update(make_params)
+            @make.save
+            redirect_to "/ads/#{@ad.id}"
+        else 
+            flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
+            render 'edit'
+        end
    else 
-      flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
-     render 'edit'
+        flash.now[:error] = "*خطأ فى إضافة اﻹعلان"
+        render 'edit'
    end
   end
-  
-  private
-  
+
+ private
+
  def ad_params
   ad_params = params.require(:ad).permit(:title,:description, :image, :image1, :image2, :image3, :image4)
   ad_params
-  end
+ end
 
-   def vehicle_params
+ def vehicle_params
   vehicle_params = params.require(:vehicle).permit(:make,:model,:manyear,:country,:axles,:gearbox,:colour,:price)
   vehicle_params
-  end
+ end
 
-   def make_params
+ def make_params
   make_params = params[:make].permit(:new,:imported,:purchase)
   make_params
-  end
+ end
 
-   def van_params
+ def van_params
   van_params = params[:van].permit(:capacity,:mileage)
   van_params
-  end
+ end
 
-   def heavytruck_params
+ def heavytruck_params
   heavytruck_params = params[:heavytruck].permit(:capacity,:mileage)
   heavytruck_params
-  end
+ end
 
-   def semitrailer_params
+ def semitrailer_params
   semitrailer_params = params[:semitrailer].permit(:capacity)
   semitrailer_params
-  end
+ end
 
-   def semitrailertruck_params
+ def semitrailertruck_params
   semitrailertruck_params = params[:semitrailertruck].permit(:mileage)
   semitrailertruck_params
-  end
+ end
 
 
 
